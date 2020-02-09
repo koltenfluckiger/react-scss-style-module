fdf64ae8b804d24165adb29cf0039fd891824c7e
Kolten Fluckiger <wrtunder@gmail.com>
2020-02-08 18:37:49 -0700

    Updating changes

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 'use strict';
 
-exports.map = (styles, classNames) => {
+exports.map = (styles, options) => {
+  const names = typeof(options.names) === "string"
+    ? options.names.split(" ")
+    : options.names;
 
-  if (Object.keys(styles).length < 1) {
-    // new Error("Styles is empty. Please check this and try again.");
-    return "";
-  };
-  if (styles.constructor != Object) {
-    //new Error('Styles is not of an object type. Pass in the styles object from the import. EX: import styles from "./container.module.css";')
-    return "";
-  };
-  if ((typeof(classNames) != "string") && (typeof(classNames) != "array")) {
-    // new Error('Class Names is not of an string type or array. Type: ' + typeof(classNames) + '. Pass in the class names in string or array format. EX: (styles, "container red visible") or (styles, ["container", "red", "visible"]);')
-    return "";
-  };
-
-  const classNamesArray = typeof(classNames) === "string"
-    ? classNames.split(" ")
-    : classNames;
-
-  return Object.values(classNamesArray).map(cssClass => {
-    if (styles[cssClass]) {
-      return styles[cssClass]
+  const classes = names.map(css => {
+    if (styles[css]) {
+      return styles[css]
     } else {
-      return cssClass
+      return css;
     }
-  }).join(" ");
+  });
+  classes.push(options.default);
+  return classes.join(" ");
 };
